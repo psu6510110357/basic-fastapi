1dedbdc0a0ba3e514a459b834361c3b4a9194067
peterpk1 <phatsawut.phuket1@gmail.com>
2025-06-30 10:55:04 +0700

    Refactor create_item endpoint to use PUT method and accept item_id as a path parameter

--- src/main.py
+++ src/main.py
@@ -20,13 +20,9 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
 
 
 @app.get("/items/{item_id}")
