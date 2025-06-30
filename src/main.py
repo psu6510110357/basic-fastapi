7ab7a8e2d75099191be04bd2cb07c3fa6050efdf
peterpk1 <phatsawut.phuket1@gmail.com>
2025-06-30 10:58:59 +0700

    Add model retrieval endpoint and ModelName enum for model identification

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 # main.py
 
+from enum import Enum
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -38,3 +39,20 @@ async def read_user_me():
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
