ea4c17a9baf4ce929bde261ba9e4c62d1d571e6a
peterpk1 <phatsawut.phuket1@gmail.com>
2025-06-30 11:01:51 +0700

    Add update_item endpoint to modify existing items

--- src/main.py
+++ src/main.py
@@ -31,6 +31,15 @@ async def read_item(item_id: int):
     return {"item_id": item_id}
 
 
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {
+        "item_id": item_id,
+        "item_name": item.name,
+        "item_description": item.description,
+    }
+
+
 @app.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
