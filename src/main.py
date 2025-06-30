9b6f6aa27df2eb07df5d4fcb8c02a4f1045a7800
peterpk1 <phatsawut.phuket1@gmail.com>
2025-06-30 10:53:12 +0700

    Refactor create_item endpoint to return item details with optional price_with_tax calculation

--- src/main.py
+++ src/main.py
@@ -22,7 +22,11 @@ async def root():
 
 @app.post("/items/")
 async def create_item(item: Item):
-    return item
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
 
 
 @app.get("/items/{item_id}")
