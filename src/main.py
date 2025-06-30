e4357c261327fd57eb7554dd7e35c2dfb854f111
peterpk1 <phatsawut.phuket1@gmail.com>
2025-06-30 10:44:37 +0700

    Add Item model and create_item endpoint to main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,28 @@
 # main.py
 
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
 
 
 @app.get("/items/{item_id}")
